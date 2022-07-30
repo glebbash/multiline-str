2525bf6ead874af881d685e7ce2813d4e0fa828c
glebbash <glebbash@gmail.com>
2022-07-30 14:30:17 +0100

    fix: strip indentation from interpolated values

--- src/multiline-str.ts
+++ src/multiline-str.ts
@@ -18,23 +18,32 @@
  * @param strings Array of template strings.
  * @param args Template string arguments.
  */
-export function multiline(strings: TemplateStringsArray, ...args: unknown[]): string {
-  const interpolationArgs = [...args, ''];
+export function multiline(
+  strings: TemplateStringsArray,
+  ...args: unknown[]
+): string {
+  const interpolationArgs = [...args, ""];
 
   // interpolate
   const stringLines = strings
     .map(
-      (str, index) => str.replace(/\\\n[ \t]*/g, '').replace(/\\`/g, '`') + interpolationArgs[index]
+      (str, index) =>
+        str.replace(/\\\n[ \t]*/g, "").replace(/\\`/g, "`") +
+        String(interpolationArgs[index]).replace(/\n/g, `\n${TOKEN}`),
     )
-    .join('')
-    .split('\n')
+    .join("")
+    .split("\n")
     .slice(1);
 
   const lastLine = stringLines.pop() as string;
   const identation = lastLine.length;
 
   // dedent
-  return stringLines.map((str) => str.slice(identation)).join('\n');
+  return stringLines.map((str) =>
+    str.split(TOKEN).join(" ".repeat(identation)).slice(identation)
+  ).join("\n");
 }
+
+const TOKEN = "```";
 
 export { multiline as m };
